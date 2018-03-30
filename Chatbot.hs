42da00869a30018ed86d7e403d56b6181443f56e
Spiros Boosalis <SamBoosalis@gmail.com>
2018-03-29 20:14:35 -0700

    .

--- Chatbot.hs
+++ Chatbot.hs
@@ -1,3 +1,39 @@
+{-# LANGUAGE RecordWildCards, NamedFieldPuns #-}
+
+{- e.g.
+
+what is my phone
+my phone is red
+what is my phone
+my phone is good
+what is my phone
+my phone is good because it's blue
+why is my phone red
+why is my phone good
+why is my phone
+
+==>
+
+>> what is my phone
+I don't know your phone
+>> my phone is red
+>> what is my phone
+Your phone is red
+>> my phone is good
+>> what is my phone
+Your phone is good
+>> my phone is good because it's blue
+>> why is my phone red
+Your phone is not red
+>> why is my phone good
+Your phone is good because it's blue
+>> why is my phone
+I don't know why your phone is the way it is
+>> 
+
+-}
+  
+
 module Main where
 
 import           "haskeline" System.Console.Haskeline
@@ -43,13 +79,14 @@ options = [ whatIs "name" "Call me"
 
 optionToActions :: Option -> [Action]
 optionToActions Option {key=key, command=command, question=question, answer=answer} =
-  [ (command , \val ->  ChangeValue $ M.insert key val)
+  [ (command , \val ->  ChangeValue $ M.insert key (Value val ""))
   , (question, \_   ->  Say $ \vals -> 
                                 case M.lookup key vals of
-                                    Just val -> answer ++ " " ++ val
+                                    Just Value{object} -> answer ++ " " ++ object
                                     Nothing  -> "I don't know your " ++ key
     )
   ]
+
 
 sanitize :: String -> String
 sanitize s = map toLower s & trim
@@ -59,23 +96,50 @@ trim = f . f
    where f = reverse . dropWhile isSpace
 
 type Command = String
+type Result = String
 type Answer = String
 type Key = String
-type Value = String
+data Value = Value { object :: String, reason :: String }
 type Values = M.Map Key Value
 data Effect = ChangeValue (Values -> Values) | Say (Values -> Answer)
-type Action = (Command, Value -> Effect)
+type Action = (Command, Result -> Effect)
 
+specialActions:: [Action]
+specialActions =
+ [ ( "my"
+   , \cmd -> case words cmd of --NOTE changed to this because a pattern-match should be total
+       (x:"is":y:"because":zs) -> ChangeValue $ M.insert x (Value y (unwords zs))
+       (x:"is":[y])            -> ChangeValue $ M.insert x (Value y "")
+       _                       -> Say $ const "Did you mean 'my _ is _' or 'my _ is _ because _'?"
+   )
+
+ , ( "what is my"
+   , \key -> Say $ \vals ->
+        case M.lookup key vals of
+          Just Value{..} -> "Your " ++ key ++ " is " ++ object
+          Nothing  -> "I don't know your " ++ key
+   )
+
+ , ( "why is my"
+   , whyIsMy
+   )
+ ]
+
+--NOTE you guys should just use a parser library, like https://github.com/mrkkrp/megaparsec
+whyIsMy suffix = case words suffix of
+  [key, property] -> Say $ \vals ->
+        case M.lookup key vals of
+          Just Value{..} ->
+            if object == property
+            then intercalate " " ["Your", key, "is", object, "because", reason]
+            else intercalate " " ["Your", key, "is not", property]
+          Nothing ->
+            intercalate " " ["I don't know why your", key, "is", property]
+  _ -> Say $ const $ intercalate " " ["I don't know why your", suffix, "is the way it is"]
+ 
 actions :: [Action]
 actions = (map optionToActions options & concat)
-              ++ [ ("my", \val -> let [x,"is",y] = words val
-                                  in ChangeValue $ M.insert x y)
-                 , ("what is my", \key -> Say $ \vals -> 
-                                                  case M.lookup key vals of
-                                                      Just val ->  "Your " ++ key ++ " is " ++ val
-                                                      Nothing  -> "I don't know your " ++ key
-                   )
-                 ]
+  ++ specialActions
 
 ai :: Command -> Effect
 ai request =
